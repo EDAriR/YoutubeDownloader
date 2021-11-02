8d5fd4ce25bedfbc705a53717908d252b42fe980
ED <ed.ho@thinkpower-info.com>
2021-11-03 01:21:14 +0900

    add pre record script

--- function/dl_mp4.py
+++ function/dl_mp4.py
@@ -3,4 +3,4 @@ import youtube_dl
 
 ydl_opts = {'outtmpl': 'download/%(title)s.%(ext)s'}
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(['https://www.youtube.com/watch?v=qeI3CjcxZVU'])
+    ydl.download(['https://youtu.be/2rxU1iy5Cqc'])
